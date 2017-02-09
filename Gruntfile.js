63bfe7c11735c8f808e7ccb93c747833267c62cb
Johnny Souto <johnny.souto@tedbrasil.com>
2017-02-09 18:19:43 -0200

    capitulo-01
    Primeira lição mudar a cor padrão, inclui também grunt e algumas tasks, less, clean e copy

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt){
+
+    grunt.initConfig({
+
+        copy: {
+
+            public: {
+                expand: true,
+                cwd: 'public',
+                src: '**',
+                dest: 'dist'
+            }
+        },
+
+        clean: {
+
+            dist: {
+                src: 'dist'
+            }
+        },
+
+        less: {
+
+            compilar: {
+                expand: true,
+                cwd: 'public/less',
+                src: ['**/*.less'],
+                dest: 'public/css',
+                ext: '.css'
+            }
+        },
+
+        watch: {
+
+            less: {
+
+                options: {
+                    event: ['added', 'changed']
+                },
+
+                files: 'public/less/**/*.less',
+                tasks: 'less:compilar'
+            }
+        }
+
+    });
+
+    grunt.registerTask('monitoramento', ['watch', 'dist']);
+    grunt.registerTask('dist', ['clean', 'copy']);
+    grunt.registerTask('default', ['dist']);
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+}
